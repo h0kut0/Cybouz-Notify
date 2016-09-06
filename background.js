1fa2a283c39dc11018c5fe1b3041809bf235f02c
hkatou <kato.hokuto@colon.ne.jp>
2016-09-06 17:06:58 +0900

    update

--- background.js
+++ background.js
@@ -1,22 +1,39 @@
-function showNotification() {
-  var options = {
-    type: "basic",
-    title: "kurt cobain",
-    message: "出社ボタンは押したかい？",
-    contextMessage: "Cybouz Notify",
-    iconUrl: "kurt.jpg",
-    requireInteraction: true
-  };
+var options = {
+  type: "basic",
+  title: "kurt cobain",
+  message: "出社ボタンは押したかい？",
+  contextMessage: "Cybouz Notify",
+  iconUrl: "kurt.jpg",
+};
 
-  //notification options set
-  chrome.notifications.create(options, callback);
-  //notification set
-}
+chrome.notifications.create(options, callback);
 
 function callback() {
-  chrome.notifications.onClicked.addListener(function(notificationId) {
+  chrome.notifications.onClicked.addListener(function() {
     window.open('https://cb.colon.ne.jp/cbag/ag.cgi');
-  })
+  });
 }
 
-showNotification();
+// function showNotification() {
+//   var options = {
+//     type: "basic",
+//     title: "kurt cobain",
+//     message: "出社ボタンは押したかい？",
+//     contextMessage: "Cybouz Notify",
+//     iconUrl: "kurt.jpg",
+//     requireInteraction: true
+//   };
+
+//   //notification options set
+//   chrome.notifications.create(options, callback);
+//   //notification set
+// }
+
+// function callback() {
+//   chrome.notifications.onClicked.addListener(function(notificationId) {
+//     window.open('https://cb.colon.ne.jp/cbag/ag.cgi');
+//     chrome.notifications.clear('notificationId', function(){});//this is working perfectly
+//   })
+// }
+
+// showNotification();
