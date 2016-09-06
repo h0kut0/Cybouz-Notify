03a5253459dbba7a350791da9a349028fba41ac2
hkatou <kato.hokuto@colon.ne.jp>
2016-09-06 17:22:23 +0900

    update

--- background.js
+++ background.js
@@ -4,15 +4,14 @@ var options = {
   message: "出社ボタンは押したかい？",
   contextMessage: "Cybouz Notify",
   iconUrl: "kurt.jpg",
-  requireInteraction: true,
-  isClickable: true
+  requireInteraction: true
 };
 
-chrome.notifications.create('id1', options, callback);
+chrome.notifications.create('notificationId', options, callback);
 
 function callback() {
   chrome.notifications.onClicked.addListener(function() {
     window.open('https://cb.colon.ne.jp/cbag/ag.cgi');
-    chrome.notifications.clear('id1', function() {});
+    chrome.notifications.clear('notificationId', function() {});
   });
 }
