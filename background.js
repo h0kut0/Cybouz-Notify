b00d15ccdad0a10cdd5819fbde851e54c6f9ff14
hkatou <kato.hokuto@colon.ne.jp>
2016-09-06 17:15:50 +0900

    update

--- background.js
+++ background.js
@@ -4,36 +4,15 @@ var options = {
   message: "出社ボタンは押したかい？",
   contextMessage: "Cybouz Notify",
   iconUrl: "kurt.jpg",
+  requireInteraction: true,
+  isClickable: true
 };
 
-chrome.notifications.create(options, callback);
+chrome.notifications.create('id1', options, callback);
 
 function callback() {
   chrome.notifications.onClicked.addListener(function() {
     window.open('https://cb.colon.ne.jp/cbag/ag.cgi');
+    chrome.notifications.clear('id1', function() {});
   });
 }
-
-// function showNotification() {
-//   var options = {
-//     type: "basic",
-//     title: "kurt cobain",
-//     message: "出社ボタンは押したかい？",
-//     contextMessage: "Cybouz Notify",
-//     iconUrl: "kurt.jpg",
-//     requireInteraction: true
-//   };
-
-//   //notification options set
-//   chrome.notifications.create(options, callback);
-//   //notification set
-// }
-
-// function callback() {
-//   chrome.notifications.onClicked.addListener(function(notificationId) {
-//     window.open('https://cb.colon.ne.jp/cbag/ag.cgi');
-//     chrome.notifications.clear('notificationId', function(){});//this is working perfectly
-//   })
-// }
-
-// showNotification();
